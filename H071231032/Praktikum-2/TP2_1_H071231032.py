f09d5957ae05b45b8e5f5508892c9379ddbef92c
MQAFSI23 <fifaizmqa23h@student.unhas.ac.id>
2023-09-18 10:48:54 +0800

    Revisi lagi

--- H071231032/Praktikum-2/TP2_1_H071231032.py
+++ H071231032/Praktikum-2/TP2_1_H071231032.py
@@ -2,49 +2,49 @@
 # H071231032
 # Tugas Praktikum Pekan Kedua
 print("\nMenghitung BMI\n==============")
-masukan = input("\nPilih jenis kelamin Anda:\n1. Pria\n2. Wanita\n")
-if masukan.isdigit():
-    gender = int(masukan)
+gender = input("\nPilih jenis kelamin Anda:\n1. Pria\n2. Wanita\n")
+if gender.isdigit() == False:
+    print("\nInvalid input.\n")
+    quit()
+gender = int(gender)
+if gender < 1 or gender > 2:
+    print("\nJumlah jenis kelamin hanyalah 2 (dua).\nJika kurang atau lebih dari itu, saya sarankan Anda ke RSJ.\n")
+    quit()
     
-    if 1 <= gender <=2:
-        match gender:
-            case 1:
-                tinggiCM = float(input("\nMasukkan tinggi badan Anda (cm): "))
-                berat = float(input("Masukkan berat badan Anda (kg): "))
-                tinggiM = tinggiCM / 100
-                bmi = berat/(tinggiM**2)
-                ket = ""
-                if bmi < 18:
-                    ket += "Underweight"
-                elif 18 <= bmi < 24:
-                    ket += "Normal"
-                elif 24 <= bmi < 27:
-                    ket += "Overweight"
-                else:
-                    ket += "Obese"
-            
-                print(f"\nAnda tergolong {ket} dengan BMI {bmi:.2f}")
+tinggi = float(input("\nMasukkan tinggi badan Anda (cm): ")) / 100
+if tinggi <= 0:
+    print("\nInvalid input.\n")
+    quit()
 
-            case 2:
-                tinggiCM = float(input("\nMasukkan tinggi badan Anda (cm): "))
-                berat = float(input("Masukkan berat badan Anda (kg): "))
-                tinggiM = tinggiCM / 100
-                bmi = berat/(tinggiM**2)
-                ket = ""
-                if bmi < 17:
-                    ket += "Underweight"
-                elif 17 <= bmi < 24:
-                    ket += "Normal"
-                elif 24 <= bmi < 28:
-                    ket += "Overweight"
-                else:
-                    ket += "Obese"
+berat = float(input("Masukkan berat badan Anda (kg): "))
+if berat <= 0:
+    print("\nInvalid input.\n")
+    quit()
 
-                print(f"\nAnda tergolong {ket} dengan BMI {bmi:.2f}")
-    
-            case _:
-                print("Invalid input.")
-    else:
-        print("\nJumlah jenis kelamin hanyalah 2 (dua).\nJika kurang atau lebih dari itu, saya sarankan Anda ke RSJ.")
-else:
-    print("\nInvalid input.")
+bmi = berat/(tinggi**2)
+
+match gender:
+    case 1:
+        if bmi < 18:
+            ket = "Underweight"
+        elif 18 <= bmi < 24:
+            ket = "Normal"
+        elif 24 <= bmi < 27:
+            ket = "Overweight"
+        else:
+            ket = "Obese"
+
+    case 2:
+        if bmi < 17:
+            ket = "Underweight"
+        elif 17 <= bmi < 24:
+            ket = "Normal"
+        elif 24 <= bmi < 28:
+            ket = "Overweight"
+        else:
+            ket = "Obese"
+
+    case _:
+        print("\nInvalid input.\n")
+
+print(f"\nAnda tergolong {ket} dengan BMI {bmi:.2f}")
