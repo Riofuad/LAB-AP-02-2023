4a6a64c46776481c9dde1c4f3b398b119d94cb47
MQAFSI23 <fifaizmqa23h@student.unhas.ac.id>
2023-09-20 14:10:04 +0800

    Menambahkan Tugas Praktikum Kedua

--- H071231032/Praktikum-2/TP2_2_H071231032.py
+++ H071231032/Praktikum-2/TP2_2_H071231032.py
@@ -3,62 +3,25 @@
 # Tugas Praktikum Pekan Kedua
 
 print("\nPencocokan Organisme\n====================")
-
-a = input("\nMasukkan inputan pertama: ")
-a = a.lower()
-if a.isalpha():
-    b = input("Masukkan inputan kedua: ")
-    b = b.lower()
-    if b.isalpha():
-        c = input("Masukkan inputan ketiga: ")
-        c = c.lower()
-        if c.isalpha():
-            match a:
-                case 'vertebrado':
-                    match b:
-                        case 'ave':
-                            match c:
-                                case 'carnivoro':
-                                    print('agula')
-                                case 'onivoro':
-                                    print('pomba')
-                                case _:
-                                    print('\nInvalid input.\n')
-                        case 'mamifero':
-                            match c:
-                                case 'onivoro':
-                                    print('homem')
-                                case 'herbivoro':
-                                    print('vaca')
-                                case _:
-                                    print('\nInvalid input.\n')
-                        case _:
-                            print('\nInvalid input.\n')
-                case 'invertebrado':
-                    match b:
-                        case 'inseto':
-                            match c:
-                                case 'hematofago':
-                                    print('pulga')
-                                case 'herbivoro':
-                                    print('lagarta')
-                                case _:
-                                    print('\nInvalid input.\n')
-                        case 'anelideo':
-                            match c:
-                                case 'hematofago':
-                                    print('sanguessuga')
-                                case 'onivoro':
-                                    print('minhoca')
-                                case _:
-                                    print('\nInvalid input.\n')
-                        case _:
-                            print('\nInvalid input.\n')
-                case _:
-                    print('\nInvalid input.\n')
-        else:
-            print('\nInvalid input.\n')
-    else:
+a = input("\nMasukkan inputan pertama: ").lower()
+b = input("Masukkan inputan kedua: ").lower()
+c = input("Masukkan inputan ketiga: ").lower()
+match a,b,c:
+    case 'vertebrado','ave','carnivoro':
+        print('\nagula\n')
+    case 'vertebrado','ave','onivoro':
+        print('\npomba\n')
+    case 'vertebrado','mamifero','onivoro':
+        print('\nhomem\n')
+    case 'vertebrado','mamifero','herbivoro':
+        print('\nvaca\n')
+    case 'invertebrado','inseto','hematofago':
+        print('\npulga\n')
+    case 'invertebrado','inseto','herbivoro':
+        print('\nlagarta\n')
+    case 'invertebrado','anelideo','hematofago':
+        print('\nsanguessuga\n')
+    case 'invertebrado','onivoro','hematofago':
+        print('\nminhoca\n')
+    case _:
         print('\nInvalid input.\n')
-else:
-    print('\nInvalid input.\n')
--- H071231032/Praktikum-2/TP2_3_H071231032.py
+++ H071231032/Praktikum-2/TP2_3_H071231032.py
@@ -1,13 +1,12 @@
 # Muhammad Qaffal Al Fifaiz | H071231032 | Tugas Praktikum Pekan Kedua
 
-print("\nPenghitung Jumlah Tagihan Listrik\n=================================\n")
+print(f"\n{'='*33}\nPenghitung Jumlah Tagihan Listrik\n{'='*33}\n")
 
 # Input golongan dan penanganan traceback-nya
-gol = input("Pilih golongan:\n1. R1\t4. B2\t7. P1\n2. R2\t5. B3\n3. R3\t6. I3\n")
-if gol.isdigit() == False or gol not in ('1','2','3','4','5','6','7'):
-    print("\nInvalid data.\n")
-    quit()
-gol = int(gol)
+gol = input("Pilih golongan:\n1. R1\t4. B2\t7. P1\n2. R2\t5. B3\n3. R3\t6. I3\n").upper()
+# if gol not in ['R1','R2','R3','B2','B3','I3','P1']:
+#     print("\nInvalid data.\n")
+#     quit()
 
 # Input daya dan penanganan traceback-nya
 daya = input("\nDaya: ")
@@ -18,53 +17,53 @@ daya = int(daya)
 
 # Penentuan tarif per kWh
 match gol:
-    case 1:
+    case 'R1':
         if daya == 900:
-            TARIF = float(1352)
+            TARIF = 1352
         elif daya == 1300 or daya == 2200:
-            TARIF = float(1444.70)
+            TARIF = 1444.70
         else:
             print("\nInvalid data.\n")
             quit()
                 
-    case 2:  
+    case 'R2':  
         if 3500 <= daya <= 5500:
-            TARIF = float(1699.53)
+            TARIF = 1699.53
         else:
             print("\nInvalid data.\n")
             quit()
 
-    case 3:     
+    case 'R3':     
         if daya >= 6600:
-            TARIF = float(1699.53)
+            TARIF = 1699.53
         else:
             print("\nInvalid data.\n")
             quit()
 
-    case 4: 
+    case 'B2': 
         if 6600 <= daya <= 200000:
-            TARIF = float(1444.70)
+            TARIF = 1444.70
         else:
             print("\nInvalid data.\n")
             quit()
 
-    case 5:        
+    case 'B3':        
         if daya > 200000:
-            TARIF = float(1114.74)
+            TARIF = 1114.74
         else:
             print("\nInvalid data.\n")
             quit()
 
-    case 6:      
+    case 'I3':      
         if daya >= 200000:
-            TARIF = float(1314.12)
+            TARIF = 1314.12
         else:
             print("\nInvalid data.\n")
             quit()
 
-    case 7:        
+    case 'P1':        
         if 6600 <= daya <= 200000:
-            TARIF = float(1523.28)
+            TARIF = 1523.28
         else:
             print("\nInvalid data.\n")
             quit()
@@ -87,4 +86,4 @@ tagihan = float(TARIF * pakai)
 depan,koma = str(tagihan).split('.')
 depan = int(depan); koma = koma[:2]
 depan = f"{depan:,}".replace(',','.')
-print(f'\nJumlah tagihan Anda sebesar: Rp{depan},{koma}\n')
+print(f'\nJumlah tagihan Anda sebesar: Rp. {depan},{koma}\n')
