02f9bf0b88c6b2dcf5cca4cc86c3466bc6850643
harmeliayra17 <harmeliarahmatika17@gmail.com>
2023-09-28 13:53:38 +0800

    H071231079_TP2_3

--- H071231079/Praktikum-2/TP2_3_H071231079.py
+++ H071231079/Praktikum-2/TP2_3_H071231079.py
@@ -16,25 +16,37 @@ match g, d:
         if daya >=3500 and daya <=5500:
             t = p * 1699.53
             print(f"Jumlah Tagihan Anda : Rp, {round(t,2):,}".replace(",","x").replace(".",",").replace("x","."))
+        else:
+            print("Invalid Input!")
     case "R3", daya:
         if daya >=6600:
             t = p * 1699.53
             print(f"Jumlah Tagihan Anda : Rp, {round(t,2):,}".replace(",","x").replace(".",",").replace("x","."))
+        else:
+            print("Invalid Input!")
     case "B2", daya:
         if daya >=6600 and daya <=200000:
             t = p * 1444.70
             print(f"Jumlah Tagihan Anda : Rp, {round(t,2):,}".replace(",","x").replace(".",",").replace("x","."))
+        else:
+            print("Invalid Input!")
     case "B3", daya:
         if daya >200000:
             t = p * 1114.74
             print(f"Jumlah Tagihan Anda : Rp, {round(t,2):,}".replace(",","x").replace(".",",").replace("x","."))
+        else:
+            print("Invalid Input!")
     case "I3", daya:
         if daya >=200000:
             t = p * 1314.12
             print(f"Jumlah Tagihan Anda : Rp, {round(t,2):,}".replace(",","x").replace(".",",").replace("x","."))
+        else:
+            print("Invalid Input!")
     case "P1", daya:
         if daya >=6600 and daya <=200000:
             t = p * 1523.28
             print(f"Jumlah Tagihan Anda : Rp, {round(t,2):,}".replace(",","x").replace(".",",").replace("x","."))
+        else:
+            print("Invalid Input!")
     case _ :
         print("Invalid Input!")
