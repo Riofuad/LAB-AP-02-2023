88e8ff0b5d4a8a17a7693e0b1fff8f18fcfb1bd1
DissentPhase <nfahrisalim@gmail.com>
2023-09-28 15:22:14 +0800

    H071231031 -Revisi Tugas TP2 no. 3

--- H071231031/Praktikum-2/TP2_3_H071231031.py.py
+++ H071231031/Praktikum-2/TP2_3_H071231031.py.py
@@ -4,9 +4,14 @@ pakai = int(input("Pemakaian (kWh): "))
 
 tarif_per_kWh = 0.0
 
-if gol == "R1" and (daya == 900 or daya == 1300 or daya == 2200):
-    tarif_per_kWh = 1352
-elif (gol == "R2" or gol == "R3") and (3500 <= daya <= 5500):
+if gol == "R1" :
+    if daya == 900 :
+        tarif_per_kWh = 1352
+    elif daya == 1300 or daya == 2200:
+        tarif_per_kWh = 1444.70
+elif gol == "R2" and (3500 <= daya <= 5500):
+    tarif_per_kWh = 1699.53
+elif gol == "R3" and (daya >= 6600):
     tarif_per_kWh = 1699.53
 elif gol == "B2" and (6600 <= daya <= 200000):
     tarif_per_kWh = 1444.7
@@ -18,7 +23,7 @@ elif gol == "P1" and (6600 <= daya <= 200000):
     tarif_per_kWh = 1523.28
 
 if tarif_per_kWh == 0.0:
-    print("Data tidak valid")
+    print("Invalid Input!")
 else:
     total = pakai * tarif_per_kWh
     rounded_total = round(total, 2)
