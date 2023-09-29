a2f07c5917bb179ade498d4551c57f8000774beb
NUGRAHSTAR <anugrahstarlight@gmail.com>
2023-09-29 16:39:45 +0800

    perbaikan lagi

--- H071231028/Praktikum-3/TP3_2_H071231028.py
+++ H071231028/Praktikum-3/TP3_2_H071231028.py
@@ -4,7 +4,7 @@ kembalian = money_d - price_b
 pecahan_money = [100000, 50000, 20000, 10000, 5000, 2000, 1000,]
 
 if kembalian < 0:
-    print("uang kamu tidak cuku,maling lagi sana")
+    print("Uang Anda tidak cukup")
 else:
     for pecahan in pecahan_money:
         jumlah = kembalian // pecahan
