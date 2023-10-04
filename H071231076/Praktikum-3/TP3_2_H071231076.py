1ce35bbda6c510814a41431e64fdc92d45f547a2
Adrian29-gpu <144887066+Adrian29-gpu@users.noreply.github.com>
2023-10-04 18:29:14 +0800

    Add files via upload

--- H071231076/Praktikum-3/TP3_2_H071231076.py
+++ H071231076/Praktikum-3/TP3_2_H071231076.py
@@ -4,7 +4,11 @@ uang = int(input())
 kembalian = uang - harga
 pecahan_uang = [100000, 50000, 20000, 10000, 5000, 2000, 1000]
 
-for pecahan in pecahan_uang:
-    jumlah = kembalian // pecahan
-    kembalian %= pecahan
-    print(f"{jumlah} uang sejumlah Rp.{pecahan}")
+if kembalian < 0 :
+    print("Uang tidak cukup")
+    
+else :
+    for pecahan in pecahan_uang:
+        jumlah = kembalian // pecahan
+        kembalian %= pecahan
+        print(f"{jumlah} uang sejumlah Rp.{pecahan}")
