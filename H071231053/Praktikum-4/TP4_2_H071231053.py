6d28e2c86516cf321273ce9bdaab11e2fcd5d7b0
H071231053_Fathan wibowo <130441901+Dworzt@users.noreply.github.com>
2023-10-10 23:06:50 +0800

    Update TP4_2_H071231053.py
    revisi v2 untuk output type hinting

--- H071231053/Praktikum-4/TP4_2_H071231053.py
+++ H071231053/Praktikum-4/TP4_2_H071231053.py
@@ -1,12 +1,13 @@
-def kata(word:str):
-    bacaan = word.lower().replace(' ','')
-    try:   
-        if bacaan == bacaan[ ::-1]:
+def kata(word: str) -> str:
+    bacaan = word.lower().replace(' ', '')
+    try:
+        if bacaan == bacaan[::-1]:
             return "Palindrom"
         else:
             return "Bukan Palindrom"
     except TypeError:
         print("type error")
+
 input_word = input("Masukkan sebuah kata: ")
 
 hasil = kata(input_word)
