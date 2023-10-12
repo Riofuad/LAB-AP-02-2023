5132551bf0af55681b4f4f8b4ff53edb6233ec17
H071231053_Fathan wibowo <130441901+Dworzt@users.noreply.github.com>
2023-10-12 21:49:50 +0800

    menambahkan tugas praktikum 5

---
+++ H071231053/Praktikum-5/TP5_1_H071230153.py
@@ -0,0 +1,14 @@
+def pergabungan(kata1, kata2):
+    var = ''
+    panjang_kata = min(len(kata1), len(kata2))
+
+    for i in range(panjang_kata):
+        var += kata1[i] + kata2[-(i + 1)]
+
+    var += kata1[panjang_kata:] + kata2[panjang_kata:]
+    return var
+
+kata1 = input("Masukkan input : ")
+kata2 = input("Masukkan input : ")
+output = pergabungan(kata1,kata2)
+print(output)
