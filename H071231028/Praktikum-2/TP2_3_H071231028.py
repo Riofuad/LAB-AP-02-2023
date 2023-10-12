8b9441892a6a0be46d1d2cbba6f3ee2d38839853
Anugrah <144884493+NUGRAHSTAR@users.noreply.github.com>
2023-10-12 13:54:16 +0800

    Add files via upload

--- H071231028/Praktikum-2/TP2_3_H071231028.py
+++ H071231028/Praktikum-2/TP2_3_H071231028.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 golongan = input("Golongan = ").upper()
 daya = int(input("Daya = "))
 pemakaian = int(input("Pemakaian = "))
@@ -51,45 +51,4 @@ match golongan, daya:
             print("Invalid input!")
     case _ :
         print("Invalid input!")
-=======
-golongan = input("Golongan = ").upper()
-daya = int(input("Daya = "))
-pemakaian = int(input("Pemakaian = "))
 
-match golongan, daya:
-    case "R1", 900:
-        tagihan = pemakaian * 1352
-        print(f"Jumlah Tagihan Anda : Rp, {round(tagihan,2):,}".replace(",","x").replace(".",",").replace("x","."))
-    case "R1", 1300:
-        tagihan = pemakaian * 1444.70
-        print(f"Jumlah Tagihan Anda : Rp, {round(tagihan,2):,}".replace(",","x").replace(".",",").replace("x","."))
-    case "R1", 2200:
-        tagihan = pemakaian * 1444.70
-        print(f"Jumlah Tagihan Anda : Rp, {round(tagihan,2):,}".replace(",","x").replace(".",",").replace("x","."))
-    case "R2", daya:
-        if daya >=3500 and daya <=5500:
-            tagihan = pemakaian * 1699.53
-        print(f"Jumlah Tagihan Anda : Rp, {round(tagihan,2):,}".replace(",","x").replace(".",",").replace("x","."))
-    case "R3", daya:
-        if daya >=6600:
-            tagihan = pemakaian * 1699.53
-        print(f"Jumlah Tagihan Anda : Rp, {round(tagihan,2):,}".replace(",","x").replace(".",",").replace("x","."))
-    case "B2", daya:
-        if daya >=6600 and daya <=200000:
-            tagihan = pemakaian * 1444.70
-        print(f"Jumlah Tagihan Anda : Rp, {round(tagihan,2):,}".replace(",","x").replace(".",",").replace("x","."))
-    case "B3", daya:
-        if daya >200000:
-            tagihan = pemakaian * 1114.74
-            print(f"Jumlah Tagihan Anda : Rp, {round(tagihan,2):,}".replace(",","x").replace(".",",").replace("x","."))
-    case "I3", daya:
-        if daya >=200000:
-            tagihan = pemakaian * 1314.12
-            print(f"Jumlah Tagihan Anda : Rp, {round(tagihan,2):,}".replace(",","x").replace(".",",").replace("x","."))
-    case "P1", daya:
-        if daya >=6600 and daya <=200000:
-            tagihan = pemakaian * 1523.28
-            print(f"Jumlah Tagihan Anda : Rp, {round(tagihan,2):,}".replace(",","x").replace(".",",").replace("x","."))
-    case _ :
-        print("Invalid Input!")
->>>>>>> 36959f7e333b25e404f740e166519f59785c185c
