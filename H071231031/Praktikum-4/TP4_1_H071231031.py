84d336883ab09f1e3e795f9027753eb8de7acb10
DissentPhase <nfahrisalim@gmail.com>
2023-10-12 21:08:28 +0800

    Revisi No 1 TP4-H071231031

--- H071231031/Praktikum-4/TP4_1_H071231031.py
+++ H071231031/Praktikum-4/TP4_1_H071231031.py
@@ -1,4 +1,8 @@
 def permutasiKata(kata):
+    for karakter in kata:
+        if not ('a' <= karakter <= 'z' or 'A' <= karakter <= 'Z'):
+            print("Masukkan harus berupa huruf saja.")
+            return
     permutasi = [kata[-i:] + kata[:-i] + " | " for i in range(1, len(kata) + 1)]
     hasil = ""
     for p in permutasi:
