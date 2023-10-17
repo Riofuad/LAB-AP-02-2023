dda26f08a8b76df0ce599b09acb98b45901b8690
H071231053_Fathan wibowo <130441901+Dworzt@users.noreply.github.com>
2023-10-17 22:05:42 +0800

    revisi TP5_1_H071230153.py
    ubah sdikitt

--- H071231053/Praktikum-5/TP5_1_H071230153.py
+++ H071231053/Praktikum-5/TP5_1_H071230153.py
@@ -1,9 +1,10 @@
 def pergabungan(kata1, kata2):
     var = ''
+    kata2 = kata2[::-1]
     panjang_kata = min(len(kata1), len(kata2))
 
     for i in range(panjang_kata):
-        var += kata1[i] + kata2[-(i + 1)]
+        var += kata1[i] + kata2[i]
 
     var += kata1[panjang_kata:] + kata2[panjang_kata:]
     return var
